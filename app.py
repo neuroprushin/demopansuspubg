5d41cad10962fca1eb2b2f33964828a71e474dc8
neuroprushin <neuro.prushin@mail.ru>
2025-04-02 23:59:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -240,4 +240,4 @@ def download_logs():
     return send_file(log_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
